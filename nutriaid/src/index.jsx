305315807c38cd6c2dda76d471e3b373cb26df56
Eddy <155239294+Eddy3129@users.noreply.github.com>
2024-09-20 00:13:17 +0800

    initialized PWA

--- nutriaid/src/index.jsx
+++ nutriaid/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import App from './App';
 import { Buffer } from 'buffer';
 
 // Polyfill Buffer globally
--- nutriaid/vite.config.js
+++ nutriaid/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'build' ? '/NutriAID/' : '/',
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'prompt',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.png'],
+        manifest: './public/manifest.json'
+      })
+    ],
+    build: {
+      assetsDir: 'assets',
+    },
     server: {
       port: 3000,
       open: true,
